1468a8360a9eac68a6f67a32379ac999f33fea87
shc0743 <shc0743@outlook.com>
2022-09-20 14:58:21 +0800

    Update

---
+++ MyAutoStartByRestartManager/MyAutoStartByRestartManager/resource.h
@@ -0,0 +1,33 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MyAutoStartByRestartManager.rc 使用
+//
+#define IDC_BUTTON1                     1000
+#define IDS_STRING_APP_UUID             1201
+#define IDS_APP_TITLE                   1203
+#define IDS_STRING_WNDCLASS_EDITOR      1204
+#define IDD_MYAUTOSTARTBYRESTARTMANAGER_DIALOG 1302
+#define IDD_ABOUTBOX                    1303
+#define IDM_ABOUT                       1304
+#define IDM_EXIT                        1305
+#define IDC_MYAUTOSTARTBYRESTARTMANAGER 1309
+#define ID_MENU_FILE_NEW                32781
+#define ID_MENU_FILE_OPEN               32782
+#define ID_MENU_FILE_SAVE               32783
+#define ID_MENU_FILE_SAVEAS             32784
+#define ID_MENU_LOAD_LOAD               32792
+#define ID_MENU_UNLOAD_SAVEANDUNLOAD    32793
+#define ID_MENU_UNLOAD_UNLOADWITHOUTSAVING 32794
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32795
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
