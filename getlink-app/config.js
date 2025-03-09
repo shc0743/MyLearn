554f7593aacf945f7203ca3c766a30a1dcb903da
shc0743 <shc0743@outlook.com>
2025-03-10 00:39:31 +0800

    Add files via upload

--- getlink-app/config.js
+++ getlink-app/config.js
@@ -2,5 +2,5 @@ window.AliyunCaptchaConfig = {
     // 必填，验证码示例所属地区，支持中国内地（cn）、新加坡（sgp）
     region: "cn",
     // 必填，身份标。开通阿里云验证码2.0后，您可以在控制台概览页面的实例基本信息卡片区域，获取身份标
-    prefix: "***",
+    prefix: "1tcbuy",
 };
