a5e11d23d8c27d4ad918a0155a099c4bf2b01416
shc0743 <shc0743@outlook.com>
2025-03-29 05:10:03 +0800

    新增build.cmd文件以支持编译txtloader和txtloader-direct，优化命令行参数处理和错误提示

---
+++ txtloader/build.cmd
@@ -0,0 +1,2 @@
+@cl txtloader.cpp /EHsc /MT /D_UNICODE /DUNICODE /std:c++20 /link /MANIFEST:EMBED
+@cl txtloader-direct.cpp /EHsc /MT /D_UNICODE /DUNICODE /std:c++20 /link /MANIFEST:EMBED
--- txtloader/txtloader-direct.cpp
+++ txtloader/txtloader-direct.cpp
@@ -1,63 +1,114 @@
 #include <windows.h>
 #include <tchar.h>
 #include <stdio.h>
+#include <string>
+#include <ShObjIdl.h>
+#include <Shlwapi.h>
+using namespace std;
 
 #pragma comment(linker, "/subsystem:windows /entry:mainCRTStartup")
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#pragma region utility
+wstring s2ws(const string str) {
+	wstring result;
+	size_t len = MultiByteToWideChar(CP_ACP, 0, str.c_str(), 
+		(int)(str.size()), NULL, 0);
+	if (len < 0) return result;
+	wchar_t* buffer = new wchar_t[len + 1];
+	if (buffer == NULL) return result;
+	MultiByteToWideChar(CP_ACP, 0, str.c_str(), (int)(str.size()), 
+		buffer, (int)len);
+	buffer[len] = '\0';
+	result.append(buffer);
+	delete[] buffer;
+	return result;
+}
+wstring& str_replace(std::wstring& strBase,
+	const std::wstring strSrc,const std::wstring strDes){
+	wstring::size_type pos = 0;
+	wstring::size_type srcLen = strSrc.size();
+	wstring::size_type desLen = strDes.size();
+	pos = strBase.find(strSrc, pos);
+	while ((pos != wstring::npos))
+	{
+		strBase.replace(pos, srcLen, strDes);
+		pos = strBase.find(strSrc, (pos + desLen));
+	}
+	return strBase;
+}
+// Translate a error-code such as GetLastError() to std::wstring
+std::wstring ErrorCodeToStringW(DWORD ErrorCode);
+// Translate a error-code such as GetLastError() to std::string
+std::string  ErrorCodeToStringA(DWORD ErrorCode);
+inline std::wstring LastErrorStrW() {
+    auto error = GetLastError();
+    auto result = ErrorCodeToStringW(error);
+    SetLastError(error);
+    return result;
+}
+#pragma endregion
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Usage: %s <file> [arguments...]\n", argv[0]);
-        return 87;
+        // printf("Usage: %s <file> [arguments...]\n", argv[0]);
+        TaskDialog(NULL, NULL, L"Error: Invalid Parameter", L"Error: Invalid Parameter", 
+            L"Usage: [loader|:s] <file> [arguments...]\n\nIf argv[0] is :s, then all "
+            "TaskDialog will be blocked. Use the following syntax to do it:\n"
+            "txtloader txtloader :s", TDCBF_CANCEL_BUTTON, TD_ERROR_ICON, NULL);
+        return ERROR_INVALID_PARAMETER;
     }
+
+	bool showTaskDialog = true;
+	if (argv[0] == ":s"s) showTaskDialog = false;
 
     // 转换为宽字符路径（Windows API 推荐使用宽字符）
-    wchar_t exePath[MAX_PATH];
-    if (MultiByteToWideChar(CP_UTF8, 0, argv[1], -1, exePath, MAX_PATH) == 0) {
-        printf("Error: Failed to convert path to wide char\n");
-        return -1;
-    }
-
-    // 检查文件是否存在
-    if (GetFileAttributesW(exePath) == INVALID_FILE_ATTRIBUTES) {
-        printf("Error: File '%ls' does not exist or cannot be accessed\n", exePath);
-        return -1;
-    }
+    wstring exePath = s2ws(argv[1]);
 
     // 构建命令行参数（格式："exe_path arg1 arg2..."）
-    wchar_t cmdLine[32768] {};
+    // wchar_t cmdLine[32768] {};
+    wstring cmdLine;
     // wcscpy_s(cmdLine, MAX_PATH * 2, L"app");
 
     if (argc > 2) {
         for (int i = 2; i < argc; i++) {
-            wchar_t argWide[32768]{};
-            if (MultiByteToWideChar(CP_UTF8, 0, argv[i], -1, argWide, 32768) == 0)
-            {
-                printf("Error: Failed to convert argument to wide char\n");
-                return -1;
+            wstring argWide = s2ws(argv[i]);
+            if (i != 2) cmdLine += L" ";
+
+            // 转义双引号
+            bool shouldQuote = argWide.find_first_of(L" ") != wstring::npos || argWide.find_first_of(L"\t") != wstring::npos;
+            if (argWide.find(L"\"") != wstring::npos) {
+                argWide = str_replace(argWide, L"\"", L"\"\"");
+                shouldQuote = true;
             }
-            if (i != 2) wcscat_s(cmdLine, L" ");
-            if (wcschr(argWide, L' ') != NULL || wcschr(argWide, L'\t') != NULL) {
-                wcscat_s(cmdLine, L"\"");
-                wcscat_s(cmdLine, argWide);
-                wcscat_s(cmdLine, L"\"");
+
+            // 参数中包含空格或制表符时，用双引号括起来
+            if (shouldQuote) {
+                cmdLine += L"\"";
+                cmdLine += argWide;
+                cmdLine += L"\"";
             } else {
-                wcscat_s(cmdLine, argWide);
+                cmdLine += argWide;
             }
         }
     }
-
     // 初始化 STARTUPINFO 和 PROCESS_INFORMATION
     STARTUPINFOW si = { sizeof(si) };
     PROCESS_INFORMATION pi = { 0 };
 
     // 使用 CreateProcessW 直接运行文件（无视扩展名）
+	wchar_t* cl = new wchar_t[cmdLine.size() + 1];
+	wcscpy(cl, cmdLine.c_str());
     BOOL success = CreateProcessW(
-        exePath,        // 可执行文件路径（即使扩展名是 .txt 也能运行）
-        cmdLine,       // 完整的命令行（含参数）
+        exePath.c_str(),        // 可执行文件路径（即使扩展名是 .txt 也能运行）
+        cl,       // 完整的命令行（含参数）
         NULL,          // 进程安全属性
         NULL,          // 线程安全属性
         FALSE,         // 不继承句柄
-        CREATE_SUSPENDED,  // 先挂起进程
+        CREATE_SUSPENDED,  // 先挂起进程，以便关联 Job 对象
         NULL,          // 使用父进程环境变量
         NULL,          // 使用父进程当前目录
         &si,           // STARTUPINFO
@@ -65,20 +116,52 @@ int main(int argc, char *argv[]) {
     );
 
     if (!success) {
-        printf("Error: Failed to execute file (Error Code: %lu)\n", GetLastError());
-        return -1;
+		DWORD ec = GetLastError();
+        if (showTaskDialog) TaskDialog(NULL, NULL, L"Error: Failed to Execute File", LastErrorStrW().c_str(),
+            (L"Error Code: " + to_wstring(ec)).c_str(), TDCBF_CANCEL_BUTTON, TD_ERROR_ICON, NULL);
+        delete[] cl;
+        return ec;
     }
 
     // 恢复子进程运行（之前是 CREATE_SUSPENDED）
     ResumeThread(pi.hThread);
 
-    printf("Successfully launched: %ls\n", cmdLine);
     printf("Child process PID: %lu\n", pi.dwProcessId);
 
     // 关闭句柄（防止泄漏）
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
-
+	delete[] cl;
 
     return 0;
 }
+
+
+
+#pragma region util
+wstring ErrorCodeToStringW(DWORD ErrorCode)
+{
+    typedef LPWSTR str_t;
+    str_t LocalAddress = NULL;
+    if (NULL == ([&] {
+        __try {
+            if (!FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                FORMAT_MESSAGE_IGNORE_INSERTS |
+                FORMAT_MESSAGE_FROM_SYSTEM, NULL,
+                ErrorCode, 0, (str_t)&LocalAddress, 0, NULL)) {
+                return (str_t)NULL;
+            }
+        }
+        __except (EXCEPTION_EXECUTE_HANDLER) { return (str_t)NULL; }
+        return LocalAddress;
+        })()) {
+        SetLastError(ErrorCode);
+        return L"";
+    }
+    LocalAddress[wcslen((str_t)LocalAddress) - 1] = 0;
+    wstring szText = LocalAddress;
+    LocalFree((HLOCAL)LocalAddress);
+    SetLastError(ErrorCode);
+    return szText;
+}
+#pragma endregion
