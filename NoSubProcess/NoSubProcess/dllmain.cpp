5c0ed80e0e7c09fa65cd836d759550563774f54d
shc0743 <shc0743@outlook.com>
2026-07-26 00:54:03 +0800

    feat(NoSubProcess): add a project named NoSubProcess

---
+++ NoSubProcess/NoSubProcess/dllmain.cpp
@@ -0,0 +1,26 @@
+﻿#include <Windows.h>
+
+void fuck() {
+    PROCESS_MITIGATION_CHILD_PROCESS_POLICY f{};
+    f.NoChildProcessCreation = 1;
+	SetProcessMitigationPolicy(ProcessChildProcessPolicy, &f, sizeof f) || TerminateProcess(GetCurrentProcess(), GetLastError());
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+#ifdef AUTOMODE
+        fuck();
+#endif
+        break;
+    default:
+        break;
+    }
+    return TRUE;
+}
+
