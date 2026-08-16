d395779c15b4a146cf8a0d9d63ef5fbae592d195
shc0743 <shc0743@outlook.com>
2026-08-16 22:55:38 +0800

    chore(tonum): add benchmark mode

--- mytonum/main.cpp
+++ mytonum/main.cpp
@@ -1,12 +1,34 @@
 ﻿#include <iostream>
+#include <string>
 #include <Windows.h>
 using namespace std;
 
 unsigned long long 转换字符串为没有符号的数字(const std::wstring& 字符串, int B = 10);
 
-int main() {
-	WCHAR buffer[256]{};
+int main(int argc, char** argv) {
 	DWORD tmp{}; char tmp2[4]{};
+	if (argc > 1) {
+		if (argv[1] == "/benchmark"s) {
+			ULONGLONG n = 1000000;
+			if (argc > 2) try { n = std::stoull(argv[2]); }
+			catch (...) { cerr << "Invalid input" << endl; return 87; }
+			cout << "Running " << n << " times..." << endl;
+			time_t s = time(0);
+			cout.flush();
+			WCHAR 字符串[] = L"123456789";
+			for (ULONGLONG i = 0;i < n;++i) {
+				转换字符串为没有符号的数字(字符串);
+			}
+			time_t e = time(0);
+			cout << "s=" << s << " t=" << e << endl;
+			cerr << "Press Enter to continue...";
+			cin.getline(tmp2, 4);
+			return 0;
+		}
+	}
+	cout << "[TIP] Use /benchmark [n] to benchmark" << endl;
+
+	WCHAR buffer[256]{};
 	cout << "Please input a string and I will convert it to number!!\nYour input: ";
 	cout.flush();
 	if (!ReadConsoleW(GetStdHandle(STD_INPUT_HANDLE), buffer, 256, &tmp, nullptr)) {
